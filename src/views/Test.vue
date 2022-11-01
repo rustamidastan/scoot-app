c067aca56d1ec2e204c99a02b658c008b186caad
rustamidastan <rustami.dastan0414@gmail.com>
2022-11-01 12:01:33 +0500

    commit

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,73 +1,24 @@
 <template>
     <div>
-        <div itemscope itemtype="https://schema.org/SoftwareApplication">
-        <span itemprop="name">Angry Birds</span> - REQUIRES
-        <span itemprop="operatingSystem">ANDROID</span><br />
-        <link
-          itemprop="applicationCategory"
-          href="https://schema.org/GameApplication" />
-      
-        <div
-          itemprop="aggregateRating"
-          itemscope
-          itemtype="https://schema.org/AggregateRating">
-          RATING:
-          <span itemprop="ratingValue">4.6</span> (
-          <span itemprop="ratingCount">8864</span> ratings )
-        </div>
-      
-        <div itemprop="offers" itemscope itemtype="https://schema.org/Offer">
-          Price: $<span itemprop="price">1.00</span>
-          <meta itemprop="priceCurrency" content="USD" />
-        </div>
-      </div>
+        <div itemscope itemtype="https://schema.org/TouristAttraction https://schema.org/Event">
+            <h1><span itemprop="name">Leaning Tower of Pisa</span></h1>
+            <div>
+                <span itemprop="description">It is a twelfth-thirteenth century Romanesque tower started building by Bonanno Pisano.</span>
+                <span itemprop="location">Location</span>
+                <span itemprop="startDate">2005-08-09</span>
+            </div>
+            <div>
+                <meta itemprop="publicAccess" content="true"/>Public access: yes
+            </div>
 
-      <div itemscope itemtype="https://schema.org/Store">
-  <div>
-    <h1 itemprop="name">Delia's Daily Supplies</h1>
-    <p itemprop="description">For your daily newspapers, candies, snacks and (via our in-store pharmacy)
-      drugs and healthcare products to keep you and your family happy, healthy and supplied with all your daily needs.</p>
-    <p>Open: <span itemprop="openingHours" content="Mo-Fr 07:00-23:00">Monday-Friday 7am-11pm </span></p>
-    <p>Phone: <span itemprop="telephone" content="+1 8005551234">800-555-1234</span></p>
+            <div itemprop="aggregateRating"
+    itemscope itemtype="https://schema.org/AggregateRating">
+   Rated <span itemprop="ratingValue">3.5</span>/5
+   based on <span itemprop="reviewCount">11</span> customer reviews
   </div>
+        </div>
 
-  <div itemprop="department" itemscope itemtype="https://schema.org/Pharmacy">
-    <h2 itemprop="name">Philippa's Pharmacy</h2>
-    <p itemprop="description">A superb collection of fine pharmaceuticals for your beauty and healthcare needs,
-      a department of Delia's Drugstore. Call our desk to speak to the on-duty pharmacist any morning Monday-Thursday.</p>
-    <p>Open: <span itemprop="openingHours" content="Mo-Th 09:00-12:00">Monday-Thursday 9am-noon</span></p>
-    <p>Phone: <span itemprop="telephone" content="+155501001111">555-0100-1111</span></p>
-  </div>
-</div>
-
-      <div itemscope itemtype="https://schema.org/Product">
-
-<span itemprop="name">Lori - Bakhtiari Persian Rug 256x143</span>
-
-Color of rug:<span itemprop="color">Red</span>
-
-<img itemprop="image" src="/nmd8457-399_details.jpg" />
-
-<span itemprop="description">Persian Rug 256x143.</span>
-
-Weight:<span itemprop="weight">4 kgr</span>
-
-Carpet Id:<span>nmd8457-399</span>
-
-<div itemprop="offers" itemscope itemtype="https://schema.org/Offer">
-
-Category:<span itemprop="category">Lori - Bakhtiari</span>
-
-<meta itemprop="priceCurrency" content="GBP" />
-
-Price:<span itemprop="price">250,00</span>£
-
-<span itemprop="availability" content="out_of_stock">Sold</span>
-
-</div>
-
-</div>
+        
     </div>
-
 
 </template>
