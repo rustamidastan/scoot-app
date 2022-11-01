83ab80a7c3cb601b07a9b1120f21ef66c8fb6862
rustamidastan <rustami.dastan0414@gmail.com>
2022-11-01 09:37:42 +0500

    adding section to test page

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <!-- <div itemscope itemtype="https://schema.org/SoftwareApplication">
+        <div itemscope itemtype="https://schema.org/SoftwareApplication">
         <span itemprop="name">Angry Birds</span> - REQUIRES
         <span itemprop="operatingSystem">ANDROID</span><br />
         <link
@@ -20,12 +20,42 @@
           Price: $<span itemprop="price">1.00</span>
           <meta itemprop="priceCurrency" content="USD" />
         </div>
-      </div> -->
+      </div>
 
       <div itemscope itemtype="https://schema.org/Museum">
             <span itemprop="name">Shinjuku Gyoen</span>
             <span itemprop="address">11 Naitomachi, Shinjuku City, Tokyo 160-0014, Japan</span>
             <a href="https://www.env.go.jp/garden/shinjukugyoen/index.html" itemprop="url">Official Website of Shinjuku Gyoen</a>
       </div>
+
+      <div itemscope itemtype="https://schema.org/Product">
+
+<span itemprop="name">Lori - Bakhtiari Persian Rug 256x143</span>
+
+Color of rug:<span itemprop="color">Red</span>
+
+<img itemprop="image" src="/nmd8457-399_details.jpg" />
+
+<span itemprop="description">Persian Rug 256x143.</span>
+
+Weight:<span itemprop="weight">4 kgr</span>
+
+Carpet Id:<span>nmd8457-399</span>
+
+<div itemprop="offers"itemscope itemtype="https://schema.org/Offer">
+
+Category:<span itemprop="category">Lori - Bakhtiari</span>
+
+<meta itemprop="priceCurrency" content="GBP" />
+
+Price:<span itemprop="price">250,00</span>£
+
+<span itemprop="availability" content="out_of_stock">Sold</span>
+
+</div>
+
+</div>
     </div>
+
+
 </template>
